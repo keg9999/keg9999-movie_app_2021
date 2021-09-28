9c549d13695e5f1fb2b81620537fe230f184348a
Dawon <bdw7475@gmail.com>
2021-09-29 03:33:54 +0900

    redirecting error

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -13,15 +13,15 @@ function Movie({ title, year, summary, poster, genres }) {
                 }}
             >
             <img src={poster} alt={title} title={title} />
-            <div className="movie__data">
-                <h3 className="movie__title">{title}</h3>
-                <h5 className="movie__year">{year}</h5>
-                <ul className="movie__genres">
+            <div className="movie-data">
+                <h3 className="movie-title">{title}</h3>
+                <h5 className="movie-year">{year}</h5>
+                <ul className="movie-genres">
                     {genres.map((genre, index) => {
-                        return <li key={index} className="movie__genre">{genre}</li>
+                        return <li key={index} className="movie-genre">{genre}</li>
                     })}
                 </ul>
-                <p className="movie__summary">{summary.slice(0,180)}...</p>
+                <p className="movie-summary">{summary.slice(0,180)}...</p>
             </div>
             </Link>
         </div>
@@ -37,3 +37,4 @@ Movie.propTypes = {
 }
 
 export default Movie;
+
