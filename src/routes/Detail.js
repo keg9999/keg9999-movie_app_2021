ce963f410ed2919d1294bbfc272dc367c78d9322
Dawon <bdw7475@gmail.com>
2021-09-29 03:32:53 +0900

    Co-authored-by: Brad <Brad294@users.noreply.github.com>

--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import './Details.css';
 
 class Detail extends React.Component{
 
@@ -12,9 +13,19 @@ class Detail extends React.Component{
 
     render(){
         const { location } = this.props;
-
+        const { title, year, summary, poster, genres } = location.state;
         if( location.state ){
-            return <span> {location.state.title} </span>;
+            return (
+              <React.Fragment>
+                <div className="details-container">
+                <span className="details-title"> {title} </span>
+                <div className="details-year"> {year} </div>
+                <div className="details-summary"> {summary} </div>
+                <img className="details-image" src={poster} alt={title} title={title} />
+                <div className="details-genres"> {genres} </div>
+                </div>
+             </React.Fragment>
+            )
         }else{
             return null;
         }
